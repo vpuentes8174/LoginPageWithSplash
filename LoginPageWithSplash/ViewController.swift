fb7a8f35a8a4dca85f3ca2485cfaa42535ea33bd
SER-YCL10 <ser-ycl10@ser-ycl10s-imac.local>
2019-07-15 09:55:25 -0500

    Initial Commit

---
+++ LoginPageWithSplash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPageWithSplash
+//
+//  Created by SER-YCL10 on 7/15/19.
+//  Copyright © 2019 SER-YCL10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LoginPageWithSplashTests/LoginPageWithSplashTests.swift
@@ -0,0 +1,36 @@
+//
+//  LoginPageWithSplashTests.swift
+//  LoginPageWithSplashTests
+//
+//  Created by SER-YCL10 on 7/15/19.
+//  Copyright © 2019 SER-YCL10. All rights reserved.
+//
+
+import XCTest
+@testable import LoginPageWithSplash
+
+class LoginPageWithSplashTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
